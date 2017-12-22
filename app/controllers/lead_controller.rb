1915906e64b4449d0971523274e4196cb68cbe95
Rob Schwartz <rob.schwartz@vibes.com>
2017-12-22 13:42:07 -0500

    save incoming leads

--- app/controllers/lead_controller.rb
+++ app/controllers/lead_controller.rb
@@ -2,8 +2,10 @@ class LeadController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def accept
+    lead = params['lead']
     # main endpoint to accept data from Lead Gen websites
     p params
+    Lead.create(lead)
     # params: full name, number, email, location, web_referral, home_type, home_id
     render json: {"status":"accepted"}
   end
