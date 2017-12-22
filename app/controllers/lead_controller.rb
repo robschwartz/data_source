28461d9db88441735d61e7c629fc9d0140c2e1be
Rob Schwartz <rob.schwartz@vibes.com>
2017-12-22 13:46:27 -0500

    save incoming leads

--- app/controllers/lead_controller.rb
+++ app/controllers/lead_controller.rb
@@ -5,8 +5,14 @@ class LeadController < ApplicationController
     lead = params['lead']
     # main endpoint to accept data from Lead Gen websites
     p params
-    Lead.create(lead)
+    Lead.create(lead_params)
     # params: full name, number, email, location, web_referral, home_type, home_id
     render json: {"status":"accepted"}
+  end
+
+  private
+
+  def lead_params
+    params.require(:lead).permit(:full_name, :number, :email, :location, :web_referral, :home_type, :home_id)
   end
 end
